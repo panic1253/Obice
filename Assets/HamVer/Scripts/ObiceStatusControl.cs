e9eda8353b38e2431a0959be82d34c4346041cc4
panic1253 <43472114+panic1253@users.noreply.github.com>
2019-11-03 19:19:36 +0900

    하기싫다

---
+++ Assets/HamVer/Scripts/ObiceStatusControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObiceStatusControl : MonoBehaviour
+{
+    public float PhysicalStrength = 2500;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
