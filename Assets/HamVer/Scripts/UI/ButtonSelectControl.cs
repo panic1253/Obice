21b8a51c1be2642d725364e99eb4dee02eabf406
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-11-14 08:00:20 +0900

    초기화

---
+++ Assets/HamVer/Scripts/UI/ButtonSelectControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSelectControl : MonoBehaviour
+{
+    public AudioSource buttonSound;
+
+    public void UI_buttonSelect(int num)
+    {
+        buttonSound.Play();
+        switch (num)
+        {
+            case 0:
+                SceneManager.LoadScene("MainGameScene");
+                break;
+            case 1:
+                Application.Quit();
+                break;
+        }
+    }
+}
